0983262c5550559ea7cc00d675600a8082cd34d4
Shaikh Amjad <samjad@ad.lmkr.net>
2016-07-03 00:16:24 +0500

    GraphDatabse

---
+++ SeismicGraphDatabase/SeismicGraphDatabase/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using SeismicGraphDatabase.Commands;
+    
+
+namespace SeismicGraphDatabase
+{   
+    class MainViewModel
+    {
+        
+        RelayCommand<object> _openCommand; 
+        public ICommand OpenCommand 
+        { 
+            get 
+            { 
+                if (_openCommand == null)
+                { 
+                    _openCommand = new RelayCommand<object>(this.Open); 
+                }
+
+                return _openCommand; 
+            } 
+        }
+
+        void Open(object o)
+        {
+            int i = 0;
+        }
+ 
+    }
+}
